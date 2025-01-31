008d16450f9e349e40611bef44f92d0d15b03bf4
kurog1ri <kosenko.yosiki@gmail.com>
2025-01-31 22:18:51 +0200

    Initial Commit

---
+++ JustNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustNote
+//
+//  Created by   Kosenko Mykola on 31.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustNote/JustNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNoteApp.swift
+//  JustNote
+//
+//  Created by   Kosenko Mykola on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct JustNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
